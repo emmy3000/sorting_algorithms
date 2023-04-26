706f9a8ce21271848a73e6df9e7398f90b64b1f4
emmy3000 <emodiemeka@gmail.com>
2023-04-25 20:52:30 -0700

    Prototype Definition used for implementing a Bubble sort algorithm

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -9,11 +10,12 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
