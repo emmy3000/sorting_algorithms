e45511d0f108bdbffad1ddab0e46c40f6ce36bdd
emmy3000 <emodiemeka@gmail.com>
2023-04-27 09:15:55 -0700

    Prototypes Definitions & Implementations of Insertion sort, Selection sort and Quick sort algorithms along with Big 'O' metrics observed under certain use cases

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -15,21 +15,21 @@ void swap_nodes(listint_t **list, listint_t **node1, listint_t *node2)
 	if (list == NULL || *list == NULL || node1 == NULL
 			|| *node1 == NULL || node2 == NULL || *node1 == node2)
 		return;
-	
+
 	if ((*node1)->prev != NULL)
 		(*node1)->prev->next = node2;
 	else
 		*list = node2;
-	
+
 	node2->prev = (*node1)->prev;
 	(*node1)->prev = node2;
-	
+
 	if (node2->next != NULL)
 		node2->next->prev = *node1;
-	
+
 	(*node1)->next = node2->next;
 	node2->next = *node1;
-	
+
 	*node1 = node2->prev;
 }
 
@@ -45,15 +45,15 @@ void swap_nodes(listint_t **list, listint_t **node1, listint_t *node2)
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *marker, *next_node;
-	
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	
+
 	for (current = (*list)->next; current != NULL; current = next_node)
 	{
 		next_node = current->next;
 		marker = current->prev;
-		
+
 		while (marker != NULL && current->n < marker->n)
 		{
 			swap_nodes(list, &marker, current);
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -16,7 +16,7 @@ void selection_sort(int *array, size_t size)
 	if (array == NULL || size <= 1)
 		return;
 
-	temp = (int*)malloc(sizeof(int) * size);
+	temp = (int *)malloc(sizeof(int) * size);
 
 	if (temp == NULL)
 		return;
